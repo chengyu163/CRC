8268bee807f419a63160404bdab9dba215271a65
Tiago João Gaspar Ribeiro de Oliveira <ist186517@pistachio.scdeec.tecnico.ulisboa.pt>
2019-10-09 19:04:07 +0100

    done: GraphAnalysis.averageDegree and GraphAnalysis.degreeDistribution

--- src/main/java/com/ist/rc/GraphAnalysis.java
+++ src/main/java/com/ist/rc/GraphAnalysis.java
@@ -1,27 +1,39 @@
 package com.ist.rc;
 import edu.uci.ics.jung.graph.Hypergraph;
 import java.util.ArrayList;
-
+import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
 /*
 * Concentrate the main metrics (Jung has them dispersed 
 * and some not completely done.) in one class. As well, as
 * some other utility functions we might find convinient.
 */
-public class GraphAnalysis {
+public class GraphAnalysis<V,E> {
 
 	Hypergraph g;
-
+	ArrayList<Integer> degreeDistribution = null;
 	public GraphAnalysis(Hypergraph graph){
 		g = graph;
 	}
 
     public double averageDegree(){
-    	return 0.0;
+    	if(averageDegree == null)
+    		averageDegree = 2*g.getEdgeCount()/g.getVertexCount();
+    	return averageDegree;
+
     }
 
-    /*Doesn't need to be arrayList*/
+    /*Degree Distribution absolute format*/
     public ArrayList degreeDistribution(){
-    	return null;
+    	if(degreeDistribution == null){
+    		ArrayList list = new ArrayList(g.getVertices());
+    		ArrayList degrees = new ArrayList(g.getVertexCount());
+   			for(int i=0; i<list.size(); i++){
+    			degrees.set(g.getNeighborCount(list.get(i)),degrees.get(i)++);
+    		}
+    	}
+    	return degreeDistribution;
     }
 
     public double averagePathLength(){
