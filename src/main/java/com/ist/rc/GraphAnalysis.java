278e3ea4ffa00a3e65668f91ec2918b2b106a4e6
Tiago João Gaspar Ribeiro de Oliveira <ist186517@pistachio.scdeec.tecnico.ulisboa.pt>
2019-10-09 20:12:07 +0100

    some comments to GraphAnalysis skeleton

--- src/main/java/com/ist/rc/GraphAnalysis.java
+++ src/main/java/com/ist/rc/GraphAnalysis.java
@@ -1,24 +1,32 @@
 package com.ist.rc;
-import edu.uci.ics.jung.graph.Hypergraph;
+import edu.uci.ics.jung.graph.Graph;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.HashMap;
+import edu.uci.ics.jung.algorithms.shortestpath.DistanceStatistics;
+import edu.uci.ics.jung.algorithms.metrics.Metrics;
 /*
 * Concentrate the main metrics (Jung has them dispersed 
 * and some not completely done.) in one class. As well, as
 * some other utility functions we might find convinient.
+*
+* Note: It is assumed who uses this class knows V and E classes
+* from graph g. 
 */
-public class GraphAnalysis<V,E> {
+public class GraphAnalysis<V,E>{
 
-	Hypergraph g;
+	Graph g;
 	ArrayList<Integer> degreeDistribution = null;
-	public GraphAnalysis(Hypergraph graph){
+	double apl = -1;
+	Map<V,Double> clusteringCoefficients = null;
+	double averageDegree = -1;
+	public GraphAnalysis(Graph graph){
 		g = graph;
 	}
 
     public double averageDegree(){
-    	if(averageDegree == null)
+    	if(averageDegree == -1)
     		averageDegree = 2*g.getEdgeCount()/g.getVertexCount();
     	return averageDegree;
 
@@ -28,20 +36,27 @@ public class GraphAnalysis<V,E> {
     public ArrayList degreeDistribution(){
     	if(degreeDistribution == null){
     		ArrayList list = new ArrayList(g.getVertices());
-    		ArrayList degrees = new ArrayList(g.getVertexCount());
+    		ArrayList<Integer> degrees = new ArrayList(g.getVertexCount());
+    		for( int i=0; i<degrees.size(); i++){ //Note:I don't like this for.
+    			degrees.set(i,0);
+    		}
    			for(int i=0; i<list.size(); i++){
-    			degrees.set(g.getNeighborCount(list.get(i)),degrees.get(i)++);
+   				int incDegree= degrees.get(i) + 1; 
+    			degrees.set(g.getNeighborCount(list.get(i)),incDegree);
     		}
     	}
     	return degreeDistribution;
     }
 
+    /* NOTE: diameter should also be a cool thing to calculate */
     public double averagePathLength(){
+    /*	use DistanceStatistics.averageDistances(g)? */
     	return 0.0;
     }
 
     public double clusteringCoefficient(){
-    	return 0.0;
+	/* use Metrics.clusteringCoefficients(g)? */
+		return 0.0; 
     }
 
     /*Suppose to put up a graphic or something about the degree distribution*/
