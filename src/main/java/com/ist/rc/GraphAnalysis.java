3d499e482477613a4c350fba3ec20e5c9ea48d54
Yu <fc49444@alunos.fc.ul.pt>
2019-10-10 21:02:12 +0100

    completing metrics functions

--- src/main/java/com/ist/rc/GraphAnalysis.java
+++ src/main/java/com/ist/rc/GraphAnalysis.java
@@ -1,8 +1,18 @@
 package main.java.com.ist.rc;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
+import org.apache.commons.collections15.Transformer;
+
+import edu.uci.ics.jung.algorithms.metrics.Metrics;
+import edu.uci.ics.jung.algorithms.shortestpath.DistanceStatistics;
+import edu.uci.ics.jung.algorithms.shortestpath.UnweightedShortestPath;
 import edu.uci.ics.jung.graph.Graph;
+import edu.uci.ics.jung.graph.util.Graphs;
 /*
 * Concentrate the main metrics (Jung has them dispersed 
 * and some not completely done.) in one class. As well, as
@@ -14,15 +24,26 @@ import edu.uci.ics.jung.graph.Graph;
 public class GraphAnalysis<V,E>{
 
 	Graph<Integer, Integer> g;
-	ArrayList<Integer> degreeDistribution = null;
+	List<Integer> degreeDistribution = null;
 	double apl = -1;
-	Map<V,Double> clusteringCoefficients = null;
+	Map<Integer,Double> clusteringCoefficients = null;
+	double averageClusteringCoefficient = 0;
 	double averageDegree = -1;
+	//averagePathLength
 	public GraphAnalysis(Graph<Integer, Integer> graph){
 		g = graph;
+		degreeDistribution = degreeDistribution();
+		clusteringCoefficients = clusteringCoefficient();
+		averageClusteringCoefficient = averageClusteringCoefficient();
+		averageDegree = averageDegree();
 	}
 
-    public double averageDegree(){
+    private double averageDegree() {
+		return degreeDistribution.stream()
+				.mapToInt(Integer::intValue).average().getAsDouble();
+	}
+
+	public double getAverageDegree(){
     	if(averageDegree == -1)
     		averageDegree = 2*g.getEdgeCount()/g.getVertexCount();
     	return averageDegree;
@@ -30,7 +51,7 @@ public class GraphAnalysis<V,E>{
     }
 
     /*Degree Distribution absolute format*/
-    public ArrayList<Integer> degreeDistribution(){
+    public List<Integer> degreeDistribution(){
     	if(degreeDistribution == null){
     		ArrayList<Integer> list = new ArrayList<Integer>(g.getVertices());
     		ArrayList<Integer> degrees = new ArrayList<Integer>(g.getVertexCount());
@@ -46,14 +67,25 @@ public class GraphAnalysis<V,E>{
     }
 
     /* NOTE: diameter should also be a cool thing to calculate */
-    public double averagePathLength(){
+    public Map<Integer, Integer> averagePathLength(){
     /*	use DistanceStatistics.averageDistances(g)? */
-    	return 0.0;
+    	Map<Integer, Integer> distances = (Map<Integer, Integer>) DistanceStatistics.averageDistances(g, new UnweightedShortestPath<Integer, Integer>(g));
+    	//dunno if it s corret, gonna study more and do it later
+    	return distances;
     }
 
-    public double clusteringCoefficient(){
+    public Map<Integer, Double> clusteringCoefficient(){
 	/* use Metrics.clusteringCoefficients(g)? */
-		return 0.0; 
+    	return Metrics.clusteringCoefficients(g);
+    	
+    }
+    
+    public double averageClusteringCoefficient() {
+    	int count = 0;
+    	for (Entry<Integer, Double> entry: clusteringCoefficients.entrySet()) {
+			count+=entry.getValue();
+		}
+		return count/clusteringCoefficients.size(); 
     }
 
     /*Suppose to put up a graphic or something about the degree distribution*/
