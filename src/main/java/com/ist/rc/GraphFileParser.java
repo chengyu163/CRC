4d5cbabb3a2059dcb317df999c8481797416195c
Tiago João Gaspar Ribeiro de Oliveira <ist186517@pistachio.scdeec.tecnico.ulisboa.pt>
2019-10-15 21:51:45 +0100

    GraphFileParser commented. TODO:: add vertex in first lines

--- src/main/java/com/ist/rc/GraphFileParser.java
+++ src/main/java/com/ist/rc/GraphFileParser.java
@@ -9,7 +9,40 @@ import java.io.FileNotFoundException;
 
 import edu.uci.ics.jung.graph.Graph;
 
-//nota: provavelmente nao preciso de V e E//
+
+/*****************
+  * Use createGraph to add vertexes and edges to graph passed as argument
+  *  
+  * How the vertexes/edges are created and how they are added is 
+  * the responsibility of the factories used and also passed as arguments.
+  *
+  * This parser does not depend on any specific representation of vertexes,edges or graphs.
+  *
+  ******************
+  * EXAMPLE:       *
+  ******************
+  *   if two file's segments that describe two different edges is, for instance:
+
+  *   ---file.txt---
+  *  >[Portugal][Spain]
+  *  >[Spain][France]
+  *  >
+  *     --- end ---
+
+  *   Being Portugal and Spain vertexes that are connected to each other:
+  * 
+  *   Then format in create Graph should be 
+  *   String format = "[V][V]\n"
+  *   
+  *   Any information to ignore or extra is referred as "ignores" and is
+  *   represented in the format as "I". ex. format = "[V]I[V]\n"
+  *
+  *  Don't forget "I" and "V" are upper case. And, to add the "\n" if it's case of it!
+  *
+******************* */
+
+
+
 class GraphFileParser<G extends Graph>{
   final String V = "V"; //how format defines a vertex
   final String I = "I"; //how format defines something to ignore/extra
@@ -17,45 +50,127 @@ class GraphFileParser<G extends Graph>{
   EdgeFactory eFactory;
   G graph;
 
+  /*
+  * Arguments:
+  *  _graph: graph with vertexes and edges to add
+  *  _vFactory: factory that knows how to create and add vertexes to graph
+  *  _eFactory: factory that knows how to create and add edges to graph
+  *
+  */
   public GraphFileParser(VertexFactory _vFactory, EdgeFactory _eFactory, G _graph){
     vFactory = _vFactory;
     eFactory = _eFactory;
     graph = _graph;
   }
  
+  /**
+    * Arguments:
+    *   format: the general structure of an edge in a file
+    *   filename: the file where the actual graph information is written
+    *   addIgnores: whether we want to collect the Ignores
+    *   linesToDelete: lines in the beggining of the file to dele  
+    *
+    * Return:
+    *  Doesn't return anything. The graph passed in constructor has now
+    *   the vertexes and edges described in the file "fileName"
+    *
+    * Semantics:         
+    *  The fundamental unit of a file in this context is the segments which define an edge. 
+    *  This function will iterate over them and send each one to splitVertex where the necessary
+    *  information to create the vertexes and edges will be returned.
+    *  It will then call the factories that will know how to use this information in order to
+    *  complete the graph passed in the constructor of this class.
+  */
+  public void createGraph(String format, String fileName, Boolean addIgnores, int linesToDelete){
+
+    String newRegex = Pattern.quote(format);
+    //newRegex will capture the segments in fileName which describe the edges
+    newRegex = newRegex.replaceAll("(?<!\\\\)"+V,"\\\\E[0-9a-zA-Z]*\\\\Q");
+    newRegex = newRegex.replaceAll("(?<=\\\\)"+V,V);
+    newRegex = newRegex.replaceAll("(?<!\\\\)"+I,"\\\\E.*\\\\Q");
+    File fp = new File(fileName);
+    try{
+      Scanner scan = new Scanner(fp);
+      for(int i=0; i<linesToDelete; i++){
+        scan.nextLine();
+      }
+      String group = scan.findWithinHorizon(newRegex,0);
+      while(group != null){
+        ParserInfo info = splitIntoVertex(format,group,addIgnores); 
+        Vertex[] ve = vFactory.addVertex(graph,info);
+        eFactory.addEdge(graph,info,ve);
+        group = scan.findWithinHorizon(newRegex,0);
+      }
+      scan.close();
+    } catch(FileNotFoundException e){
+      System.out.println(e);
+      System.exit(-1);
+    }
+  }
+
+	public void createGraph(String format, String fileName){
+  	createGraph(format, fileName, false);
+  }
+
+  public void createGraph(String format, String fileName, boolean addIgnores){
+    createGraph(format, fileName, false,0);
+  }
+
+
+  /**
+    * Arguments:
+    *   format: the general structure of an edge in a file
+    *   phrase: the current edge being read in file
+    *   addIgnores: whether we want to collect the information 
+    *               assigned with "I" in format
+    *
+    * Return:
+    *   class with vertex identification and Ignores, if addIgnores == True
+    *
+    * Semantics:
+    *   Each phrase describe an edge. We will iterate through the phrase
+    *   using the general expression "format" passed, which describes the
+    *   structure of the phrase, and attach all the information in an instance
+    *   of the class ParseInfo.
+    *
+  **/
   public ParserInfo splitIntoVertex(String format, String phrase,boolean addIgnores){
     format = "|"+ format + "|";  //the algorithms knows when to stop by looking ahead and behind 
     phrase = "|"+ phrase + "|";  //this is to always have something to look ahead and behind
-    format = format.toUpperCase();
+
     int phraseIndex = 0;  
-    int formatIndex = 0;
+    int formatIndex = 0; 
     int vertexNumber = 0;
     StringBuilder[] vertex = new StringBuilder[2];
     ArrayList<StringBuilder> ignoreList = new ArrayList<StringBuilder>();
     vertex[0] = new StringBuilder();
     vertex[1] = new StringBuilder();
     char toMatch;
+
     while( phraseIndex < phrase.length()){ 
-      //If supposed to look for a vertex in phrase (having a \ before is a escape character)
+      //Escape character behind V and I are completely ignored
+      if(format.charAt(formatIndex)=='\\' && (format.charAt(formatIndex+1)!=V.charAt(0) || format.charAt(formatIndex+1)!=I.charAt(0))){
+        formatIndex++;
+      }
+
       if(format.charAt(formatIndex)==V.charAt(0) && format.charAt(formatIndex-1)!='\\'){
         formatIndex++;
-        toMatch = format.charAt(formatIndex);
+        toMatch = format.charAt(formatIndex); //All the information until this character is part of the vertex name
         while(phrase.charAt(phraseIndex) != toMatch){
           vertex[vertexNumber].append(phrase.charAt(phraseIndex++)); 
         }
-        vertexNumber++; //proximo vertice
+        vertexNumber++; //O.K. we've read one vertex, next please...
       }
-      //If supposed to look for a Ignore in phrase (having a \ before is a escape character)
-      if(format.charAt(formatIndex)==I.charAt(0) && format.charAt(formatIndex)!='\\'){
+      if(format.charAt(formatIndex)==I.charAt(0) && format.charAt(formatIndex)!='\\'){ 
         formatIndex++;
         toMatch = format.charAt(formatIndex);
         StringBuilder newIgnore = new StringBuilder();
-        if(addIgnores){
-        	ignoreList.add(newIgnore);
+        if(addIgnores){ //do we want to collect this Ignore?
+          ignoreList.add(newIgnore);
         }
         while(phrase.charAt(phraseIndex) != toMatch){ 
-          if(addIgnores){
-          	newIgnore.append(phrase.charAt(phraseIndex));
+          if(addIgnores){ 
+            newIgnore.append(phrase.charAt(phraseIndex));
           }
           phraseIndex++;
         }
@@ -64,54 +179,31 @@ class GraphFileParser<G extends Graph>{
       formatIndex ++;
     }
     
-    if(addIgnores){
-    	return new ParserInfo(vertex,addIgnores,ignoreList);
+    if(addIgnores){ 
+      return new ParserInfo(vertex,addIgnores,ignoreList);
     }
     else{
-    	return new ParserInfo(vertex);
+      return new ParserInfo(vertex);
     }
   }
 
-	public void createGraph(String format, String fileName){
-  	createGraph(format, fileName, false);
-  }
-
-  //Needs emptygraph to be passed because G is not defined at compile time.
-  public void createGraph(String format, String fileName, Boolean addIgnores){
-
-
-    format = format.toUpperCase();
-    String[] splitRegex = format.split(V);
-    String newRegex = Pattern.quote(format);
-    //problem: this is not accounting for escape characters
-    newRegex = newRegex.replaceAll(V,"\\\\E[0-9a-zA-Z]*\\\\Q");
-    newRegex = newRegex.replaceAll(I,"\\\\E.*\\\\Q");
-    File fp = new File(fileName);
-    try{
-    	Scanner scan = new Scanner(fp);
-	    String group = scan.findWithinHorizon(newRegex,0);
-	    while(group != null){
-	      ParserInfo s = splitIntoVertex(format,group,addIgnores); //TODO: name s?
-	      Vertex[] ve = vFactory.addVertex(graph,s);
-	      eFactory.addEdge(graph,s,ve);
-	      group = scan.findWithinHorizon(newRegex,0);
-	      System.out.println(s);
-	    }
-	    scan.close();
-
-		} catch(FileNotFoundException e){
-   		System.out.println(e);
-   		System.exit(-1);
-   	}
-  }
-
+ /*
+   * Class to store the information received in file.
+   * Each instance belongs to each edge segment
+   * 
+   * Fields:
+   *  vertex: strings captured in segment where the letter "V" is. 
+   *          it is supposed to be the information relevant for each vertex
+   *
+   *  hasIgnores: whether we captured any Ignore information
+   *
+   *  ignoreList: information capture in segment where the letter "I" is.
+   *              it's the ignores information
+ */
   class ParserInfo{
       public StringBuilder[] vertex;
       public boolean hasIgnores;
       public ArrayList<StringBuilder> ignoreList;
-      /* Not advisable, but, if needed, you can attach anything to ParserInfo
-      to complement any other information you might want*/
-      public Object strategy  = null; 
       public ParserInfo(StringBuilder[] _vertex){
         vertex = _vertex;
         hasIgnores = false;
@@ -125,16 +217,13 @@ class GraphFileParser<G extends Graph>{
       //*change to have more than one vertex*/
       public String toString(){
         StringBuilder v = new StringBuilder();
-        v.append("(v[0],v[1]) = (");
-        v.append(vertex[0]);
-        v.append(",");
-        v.append(vertex[1]);
-        v.append(")\n");
-        v.append("IGNORELIST:\n");
+        for(int i=0; i<vertex.length; i++){
+          v.append("v["+i+"]= " + vertex[i] +"\n");
+        }
         if(hasIgnores){
+          v.append("IGNORELIST:\n");
           for(StringBuilder i: ignoreList){
-            v.append(i);
-            v.append("\n");
+            v.append(i + "\n");
           }
         }
         return v.toString();
